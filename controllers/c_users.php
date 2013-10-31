666f438936d154b8f2fa78eef0f3cc152224b771
DARE MAKUN <dremak11@gmail.com>
2013-10-31 11:26:08 -0400

    Added login function echo not responding

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -10,6 +10,10 @@ class users_controller extends base_controller {
         echo "This is the index page";
     }
 
+/*-------------------------------------------------------------------------------------------------
+        Display a form so users can sign up        
+        -------------------------------------------------------------------------------------------------*/
+
     public function signup() {
     
         # Set up the view
@@ -19,6 +23,9 @@ class users_controller extends base_controller {
     	echo $this->template;
     	
     }
+    /*-------------------------------------------------------------------------------------------------
+Process the sign up form
+-------------------------------------------------------------------------------------------------*/
     
     public function p_signup () {
     
@@ -39,14 +46,62 @@ class users_controller extends base_controller {
     }
 
     public function login() {
-        echo "This is the login page";
+        
+        $this->template->content = View::instance('v_users_login');
+        echo $this->template;
+        
     }
+
+	 /*-------------------------------------------------------------------------------------------------
+Process the login form
+-------------------------------------------------------------------------------------------------*/
+    public function p_login() {
+                 
+     # Hash the password they entered so we can compare it with the ones in the database
+        $_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
+                
+     # Set up the query to see if there's a matching email/password in the DB
+        $q =
+             'SELECT token
+              FROM users
+              WHERE email = "'.$_POST['email'].'"
+              AND password = "'.$_POST['password'].'"';
+                
+        echo $q;        
+                
+     # If there was, this will return the token        
+        $token = DB::instance(DB_NAME)->select_field($q);
+                
+     # Success
+        if($token) {
+                
+     # Don't echo anything to the page before setting this cookie!
+        setcookie('token',$token, strtotime('+1 year'), '/');
+        echo "Success. You are logged in!";
+                        
+     # Send them to the homepage
+            jRouter::redirect('/');
+                }
+                # Fail
+                else {
+                        echo "Login failed! <a href='/users/login'>Try again?</a>";                }
+        
+    }
+
+
+
+
 
     public function logout() {
         echo "This is the logout page";
     }
 
     public function profile($user_name = NULL) {
+    
+    # Only logged in users are allowed...
+                if(!$this->user) {
+                        die('Members only. <a href="/users/login">Login</a>');
+                }
     
     # Set up the view
     	$this->template->content = View::instance('v_users_profile');
@@ -71,4 +126,5 @@ class users_controller extends base_controller {
 
     }
 
-} # end of the class
+}
+# end of the class
